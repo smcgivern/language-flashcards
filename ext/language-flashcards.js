52bb6cffe22673f6dfae5cc8f55e2cd933d13102
Sean McGivern <sean@mcgivern.me.uk>
2011-12-04 21:57:53 +0100

    Load JSON from file; (very) basic flashcard game running

--- ext/language-flashcards.js
+++ ext/language-flashcards.js
@@ -1,14 +1,8 @@
-var flashcards = {
-    "Spanish": {
-        "Regular verbs": [
-            ["hablar", "to speak"],
-            ["comer", "to eat"],
-            ["beber", "to drink"]
-        ]
-    }
-};
+// Number of times each card needs to be guessed correctly.
+var repetitions = 5;
 
-var u = encodeURIComponent;
+// Placeholder for flashcards; loaded at the bottom of this file.
+var flashcards = {};
 
 function element(name, content, attributes) {
     var e = $(document.createElement(name));
@@ -19,18 +13,59 @@ function element(name, content, attributes) {
     return e;
 }
 
+function showList(language, list, cards) {
+    $('#language-title').empty().append(language);
+    $('#list-title').empty().append(list);
+
+    var cardList = cards;
+
+    for (var i = 1; i < repetitions; i++) {
+        cardList = cardList.concat(cards);
+    }
+
+    nextCard(cardList);
+}
+
+function nextCard(remainingCards) {
+    remainingCards.sort(function() { return 0.5 - Math.random(); })
+
+    currentCard = remainingCards.shift();
+
+    $('#word').empty().append(currentCard[0]);
+    $('#answer').val("").focus();
+
+    $('#card').off('submit', 'form').on('submit', 'form', function() {
+        checkAnswer(currentCard, remainingCards);
+    });
+}
+
+function checkAnswer(card, remainingCards) {
+    var answer = $('#answer').val().toLowerCase();
+
+    if (answer != card[1]) {
+        remainingCards.push(card);
+        $('#result').empty().append('Uh-uh!');
+    } else {
+        $('#result').empty().append('Woo-hoo!');
+    }
+
+    nextCard(remainingCards);
+}
+
 function showMenu() {
     var wrapper = $('#menu');
     var menu = element('ul');
 
     $.each(flashcards, function(language, lists) {
+
         var languageWrapper = element('li');
         var languageMenu = element('ul');
 
         $.each(lists, function(list, cards) {
-            var link = element('a', list, {
-                href: '#language=' + u(language) +
-                    ';list=' + u(list),
+            var link = element('span', list, { class: 'click' });
+
+            link.click(function() {
+                showList(language, list, cards);
             });
 
             languageMenu.append(element('li').append(link));
@@ -41,10 +76,14 @@ function showMenu() {
         menu.append(languageWrapper);
     });
 
-    wrapper.empty();
-    wrapper.append(element('h1', 'Menu')).append(menu);
+    wrapper.empty().append(element('h1', 'Menu')).append(menu);
 
     return wrapper;
 }
 
-$(function() { showMenu(); });
+$(function() {
+    $.getJSON('ext/flashcards.json', function(data) {
+        flashcards = data;
+        showMenu();
+    });
+});
